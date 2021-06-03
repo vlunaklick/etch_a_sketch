58ee5bda062995d7d0997656c2b893aee1128133
Valen <v.lunaklick@gmail.com>
2021-06-03 19:19:34 -0300

    Finished proyect

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ function changeBlack (e){
     let val = e.target.classList[1];
     let divSelec = document.getElementsByClassName(`${val}`)[0];
     if (colorP == "black"){
-        divSelec.style.backgroundColor = `${colorP}`;
+        divSelec.style.backgroundColor = `rgba(0,0,0)`;
     }   else if (colorP == "rainbow"){
         let rColor = Math.floor(Math.random()*255);
         let gColor = Math.floor(Math.random()*255);
         let bColor = Math.floor(Math.random()*255);
-        divSelec.style.backgroundColor = `rgb(${rColor},${gColor},${bColor})`;
+        divSelec.style.backgroundColor = `rgba(${rColor},${gColor},${bColor})`;
     } else if(colorP == "greytones"){
-        let greyColor = Number(divSelec.style.backgroundColor.slice(-4,-1));
-        console.log(greyColor)
-        divSelec.style.backgroundColor = `rgba(0,0,0,${greyColor + 0.2})`;
-    }   
+        if (divSelec.style.backgroundColor == 'rgba(0, 0, 0, 0.9)'){
+            return
+        }  else if(divSelec.style.backgroundColor.match(/rgba/)){
+            let greyS = Number(divSelec.style.backgroundColor.slice(-4, -1));
+            divSelec.style.backgroundColor = `rgba(0, 0, 0, ${greyS + 0.1})`;
+        } else {
+            divSelec.style.backgroundColor = `rgba(0, 0, 0, 0.1)`;
+        }
+    } else if(colorP == "setcolor"){
+        let hexColor = document.querySelector(`#seleColor`);
+        let hexValue = hexColor.value
+        divSelec.style.backgroundColor = `${hexValue}`;
+    }
 }
 
 function createTable(size){
@@ -26,8 +35,9 @@ function createTable(size){
         div.classList.add(`${i}`);
         div.style.width = `${siPx}px`;
         div.style.height = `${siPx}px`;
-        div.style.border = `1px solid rgba(255,255,255,0.5`;
+        div.style.border = `1px solid rgba(255,255,255,0.5)`;
         div.style.backgroundColor = "none";
+        div.style.transition = "0.2s";
         container.appendChild(div);
     }
     allDivs = Array.from(document.querySelectorAll(`.hover`));
@@ -59,6 +69,10 @@ function setRainbow(){
 
 function setGrey(){
     colorP = "greytones"
+}
+
+function setColor(){
+    colorP = "setcolor"
 }
 
 
@@ -98,7 +112,9 @@ clearBtn.addEventListener('click', resetTable);
 const blackC = document.querySelector('#black');
 const rainbowC = document.querySelector('#rainbow');
 const greysC = document.querySelector('#greys');
+const hexC = document.querySelector('#selectorCol')
 
 blackC.addEventListener('click', setBlack);
 rainbowC.addEventListener('click', setRainbow);
 greysC.addEventListener('click', setGrey);
+hexC.addEventListener('click', setColor);
